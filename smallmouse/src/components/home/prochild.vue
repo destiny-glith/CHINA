089196821a2991e7be7feaf1366456748ab88056
destiny-glith <354548385@qq.com>
2019-07-16 19:43:20 +0800

    分支第一次提交“
    git commit -m 分支第一次提交“

---
+++ smallmouse/src/components/home/prochild.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- 第一张图片 -->
+    <div class="index_banner">
+      <img :src="index_banner" alt />
+    </div>
+    <!-- 标题 -->
+    <div class="main_floor">
+      <div class="hezi"></div>
+      <p>{{main_floor}}</p>
+    </div>
+    <div class="main_index">
+      <ul>
+        <li v-for="(item,index) of bao1" :key="index">
+          <div class="itemimg">
+            <div class="bigimg">
+              <img :src="item['pic']" alt />
+            </div>
+            <div class="smallimg">
+              <img :src="samllimg" alt />
+            </div>
+          </div>
+          <p class="itemtit">{{item['alias']}}</p>
+          <p class="itemliang">{{item['name']}}</p>
+          <p class="itemscall">
+            <span></span>
+            <span></span>
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  
+</style>
--- smallmouse/src/components/home/prolist.vue
+++ smallmouse/src/components/home/prolist.vue
@@ -26,43 +26,14 @@
         <div class="main_banner"></div>
       </van-tab>
       <van-tab v-for="(item,index) of shaixuan" :key="index" :title="item">
-        <!-- <component :is="type"></component> -->
+        <Prochild />
       </van-tab>
     </van-tabs>
   </div>
 </template>
 
 <script>
-
-// <!-- 第一张图片 -->
-// <div class="index_banner">
-//   <img :src="index_banner" alt />
-// </div>
-// <!-- 标题 -->
-// <div class="main_floor">
-//   <div class="hezi"></div>
-//   <p>{{main_floor}}</p>
-// </div>
-// <div class="main_index">
-//   <ul>
-//     <li v-for="(item,index) of bao1" :key="index">
-//       <div class="itemimg">
-//         <div class="bigimg">
-//           <img :src="item['pic']" alt />
-//         </div>
-//         <div class="smallimg">
-//           <img :src="samllimg" alt />
-//         </div>
-//       </div>
-//       <p class="itemtit">{{item['alias']}}</p>
-//       <p class="itemliang">{{item['name']}}</p>
-//       <p class="itemscall">
-//         <span></span>
-//         <span></span>
-//       </p>
-//     </li>
-//   </ul>
-// </div>
+import Prochild from '@/components/home/prochild'
 import Vue from 'vue'
 import { Tab, Tabs } from 'vant'
 import { log } from 'util';
@@ -129,6 +100,9 @@ export default {
       console.log(this.type);
     }
   },
+  components: {
+    Prochild
+  }
 }
 </script>
 
