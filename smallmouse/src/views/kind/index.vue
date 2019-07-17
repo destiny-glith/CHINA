a6b449c6a383f3f5e80ef98b98884841b94fb3f7
zhangzhang107 <934003755@qq.com>
2019-07-17 20:17:23 +0800

    7月17分类商品页面

--- smallmouse/src/views/kind/index.vue
+++ smallmouse/src/views/kind/index.vue
@@ -1,12 +1,113 @@
 <template>
   <div class="container">
-    <div class="header">分类</div>
-    <div class="main">分类</div>
+    <div class="header">
+      <span>分类</span>
+      <span>发现</span>
+      <p><i class="iconfont icon-sousuo1"></i></p>
+    </div>
+    <div class="main">
+      <div class="nav">
+        <div   class="nav-left" v-for="(item, index) of listdate" :key="index">
+          占贷{{item}}
+        </div>
+      </div>
+      <div class="contentt">
+        <div v-for="(item, index) of listdata" :key="index">
+          <!-- <p><img src="../../img/man01.png" alt=""></p> -->
+          <span>图片{{item}}</span>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+      listdata: [ 1, 2, 3, 4, 5, 5, 7, 8, 91, 1, 1, 23, 4, 9, 8, 3, 4, 2, 2, 2, 2, 1, 2, 3, 4, 5, 5, 7, 8, 91, 1, 1, 23, 4, 9, 8, 3, 4, 2, 2, 2, 2 ],
+      listdate: [ 1, 2, 3, 4, 5, 5, 7, 8, 91, 1, 1, 23, 4, 9, 8, 3, 4, 2, 2, 2, 2, 1, 2, 3, 4, 5, 5, 7, 8, 91, 1, 1, 23, 4, 9, 8, 3, 4, 2, 2, 2, 2 ]
+    }
+  },
+  mounted () {
+  }
 }
 </script>
+<style lang="scss">
+@import "@/lib/reset.scss";
+.header {
+  background-color: #FFFFFF;
+  color: #000000;
+  border-bottom: 1px solid #CCCCCC;
+  @include flexbox();
+  span:nth-child(1) {
+    @include rect(1.03rem, 0.44rem);
+    @include flexbox();
+    justify-content: center;
+    margin-left: 0.44rem;
+    line-height: 0.44rem;
+  }
+   span:nth-child(2) {
+    @include rect(1.03rem, 0.44rem);
+    @include flexbox();
+    justify-content: center;
+    line-height: 0.44rem;
+  }
+  p {
+  @include flexbox();
+  @include flex();
+  justify-content: center;
+  line-height: 0.44rem;
+  color: green;
+  i {
+    font-size: 20px;
+    }
+  }
+}
+.main {
+  @include flexbox();
+  .nav {
+    // @include flexbox();
+    @include rect(20%,auto);
+    background-color: blue;
+    // flex-direction: column;
+    overflow: auto;
+    div {
+      border-bottom: 1px solid #CCCCCC;
+      @include rect(100%, 0.5rem);
+      @include flexbox();
+      justify-content: center;
+      line-height: 0.5rem;
+    }
+  }
+  .contentt {
+    @include flexbox();
+    @include rect(80%,auto);
+    background-color: yellow;
+    flex-wrap: wrap;
+    // flex-direction: column;
+    overflow: auto;
+    div {
+      @include rect(33%, 0.88rem);
+      border-bottom: 1px solid #CCCCCC;
+      p {
+        @include rect(100%, 0.68rem);
+        img {
+          @include rect(100%, 100%);
+        }
+      }
+      span {
+        @include rect(100%, 0.28rem);
+         @include flexbox();
+         justify-content: center;
+
+      }
+      &.router-link-active {
+        // li.router-link-active {}
+        @include color(#f66);
+      }
+    }
+  }
+}
+</style>
--- smallmouse/src/views/mouseshop/index.vue
+++ smallmouse/src/views/mouseshop/index.vue
@@ -1,12 +1,240 @@
 <template>
   <div class="container">
-    <div class="header">松树商城</div>
-    <div class="main">松树商城</div>
+    <!-- 头部 -->
+    <!-- 头部 -->
+    <div class="header">
+      <h3>松树币商城</h3>
+    </div>
+    <!-- 内容 -->
+    <!-- 内容 -->
+    <div class="main">
+      <!-- 头部 -->
+      <div class="head">
+        <h3>我的松鼠币</h3>
+        <span>0</span>
+        <p>松鼠好礼等你来拿~</p>
+        <div>登录/注册</div>
+      </div>
+      <!-- 第二图片 -->
+      <!-- 第二图片 -->
+      <div class="listimg">
+        <div>
+          <!-- <img src="../../img/img01.png" alt=""> -->
+          <div>
+            <p>购物袋</p>
+            <span>想要的都在这</span>
+          </div>
+        </div>
+        <div>
+          <!-- <img src="../../img/img01.png" alt=""> -->
+          <div>
+            <p>我的兑换</p>
+            <span>查看松鼠币兑换订单</span>
+          </div>
+        </div>
+      </div>
+      <!-- 一个小导航 -->
+      <!-- 一个小导航 -->
+      <div class="nav">
+        <p>兑换中心</p>
+        <span>0</span>
+      </div>
+      <!-- 内容 -->
+      <!-- 内容 -->
+      <div class="man">
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+        <div>
+            <!-- <img src="../../img/man01.png" alt=""> -->
+            <p>会员福利满148减15</p>
+            <span>每人限兑1000件</span>
+            <span>100</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
+
+<style lang="scss">
+@import "@/lib/reset.scss";
+.header {
+  background-color: #FFFFFF;
+  color: #000000;
+  border-bottom: 1px solid #CCCCCC;
+  h3 {
+    display: block;
+    font-size: 17px;
+    font-weight: 400;
+    display: flex;
+    justify-content: center;
+    line-height: 0.44rem;
+  }
+}
+.main {
+  background-color: #FFFFFF;
+  .head {
+    @include rect(100%, 1.5rem);
+    background-color: #ffff;
+    border-bottom: 1px solid #CCCCCC;
+    display: flex;
+    flex-direction: column;
+    h3 {
+      font-size: 12px;
+      font-weight: 300;
+      display: flex;
+      justify-content: center;
+      line-height: 0.44rem;
+    }
+    span {
+      font-size: 22px;
+      font-weight: 500;
+      display: flex;
+      justify-content: center;
+      line-height: 0.34rem;
+      color: red;
+    }
+    p {
+      font-size: 13px;
+      font-weight: 500;
+      display: flex;
+      justify-content: center;
+      line-height: 0.24rem;
+    }
+    div {
+      font-size: 13px;
+      font-weight: 500;
+      line-height: 0.3rem;
+      background-color: #77BC1F;
+      border-radius: 0.3rem;
+      @include rect(0.8rem, 0.3rem);
+      justify-content: center;
+      display: flex;
+      margin: 0.1rem auto;
+      color: #FFFFFF;
+    }
+  }
+  .listimg {
+    @include rect(100%, 0.38rem);
+    @include flexbox();
+    border-bottom: 1px solid #CCCCCC;
+    >div {
+      @include rect(50%, 0.38rem);
+      border-right: 1px solid #CCCCCC;
+      @include flexbox();
+      img {
+        @include rect(0.3rem, 0.38rem);
+        margin-left: 0.1rem;
+      }
+      div {
+        @include flexbox();
+        flex-direction: column;
+        margin-left: 0.1rem;
+        p {
+          font-size: 10px;
+
+        }
+        span {
+          font-size: 8px;
+        }
+      }
+    }
+  }
+  .nav {
+    @include rect(100%, 0.35rem);
+    @include flexbox();
+    background-color: #F9F9F9;
+    p {
+      @include rect(0.7rem, 0.35rem);
+      margin-left: 0.1rem;
+      @include flexbox();
+      align-items: center;
+    }
+    span {
+      @include rect(0.2rem, 0.35rem);
+      @include flexbox();
+      align-items: center;
+    }
+  }
+  .man {
+    @include rect(100%, auto);
+    @include flexbox();
+    flex-wrap: wrap;
+    background-color: #ccc;
+    >div {
+      @include rect(49.3%, 1.90rem);
+      // @include flex-direction(columns);
+      background-color: #ffff;
+      margin: 0.02rem 0;
+      img {
+        margin: 0.20rem 0.36rem 0.15rem 0.35rem;
+      }
+      p {
+        @include flexbox();
+        justify-content: center;
+        font-size: 10px;
+      }
+      span:nth-child(3) {
+        font-size: 12px;
+        display: block;
+        margin-left: 0.2rem;
+
+      }
+      span:nth-child(4) {
+        font-size: 10px;
+        display: block;
+         margin-left: 0.2rem;
+      }
+    }
+    >div:nth-child(odd) {
+      margin-right: 4px;
+    }
+  }
+
+}
+</style>
